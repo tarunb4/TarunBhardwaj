c453265bafd333a44ef70d448b4597087a439056
Tarun Bhardwaj <tarunb4@my.yorku.ca>
2026-01-27 17:10:16 -0500

    Updated Resume again

--- pages/components/About.tsx
+++ pages/components/About.tsx
@@ -71,7 +71,7 @@ const About = () => {
           </div>
           <div className="mt-6 mx-auto md:mx-0 border-2 border-white py-1 px-2 w-36 text-center font-bold">
             <a
-              href={"/Tarun_Bhardwaj_Resume_2025.pdf"}
+              href={"/Tarun_Bhardwaj_Resume_2025_UPDATED.pdf"}
               target="_blank"
               className="about__resume text-white text-l"
             >
